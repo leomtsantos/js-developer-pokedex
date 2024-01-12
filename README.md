4c77f009ce3f31bd71cc8f27a655ccf8cb0d28ec
leomtsantos <leonardo.moutinho2004@gmail.com>
2024-01-12 15:35:47 -0300

    Enviando alterações finais

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Trilha JS Developer - Pokedex
+# Minha Pokedex
+
+Uma aplicação simples de Pokedex desenvolvida durante o bootcamp Potência Tech Angular Developer - Powered by iFood, seguindo as aulas ministradas por Renan Paula. Esse projeto também é baseado no repositório [js-developer-pokedex](https://github.com/digitalinnovationone/js-developer-pokedex) da Digital Innovation One (DIO).
+
+## Descrição
+
+A Minha Pokedex é uma aplicação web que permite explorar e visualizar informações sobre diferentes pokémons. Ela utiliza a PokeAPI para obter os dados dos pokémons, exibindo seus números, nomes, tipos e imagens correspondentes.
+
+## Tecnologias utilizadas
+
+- JavaScript
+- HTML
+- CSS
+
+## Instalação
+
+1. Clone este repositório:
+
+2. Navegue até o diretório do projeto:
+
+3. Abra o arquivo `index.html` em um navegador web.
+
+## Como usar
+
+- Ao abrir a aplicação, você verá uma lista inicial de pokémons exibidos.
+- Role para baixo para carregar mais pokémons.
+- Cada item da lista exibe o número, nome, tipos e imagem do respectivo pokémon.
+- Clique no botão "Carregar Mais" para exibir mais pokémons.
+
+## Contribuição
+
+Contribuições são bem-vindas! Se você quiser contribuir para a Minha Pokedex, siga estas etapas:
+
+1. Faça um fork deste repositório.
+2. Crie uma nova branch para a sua contribuição.
+3. Faça as alterações desejadas.
+4. Faça um commit das suas alterações.
+5. Faça um push para a branch.
+6. Abra uma Pull Request neste repositório.
+
+## Autor 
+
+- [Gabriel H. L. Oliveira](https://github.com/GabrielV26)
+
+## Agradecimentos 👏
+
+Gostaria de agradecer ao instrutor Renan Paula pelo excelente conteúdo apresentado no bootcamp Potência Tech Angular Developer - Powered by iFood, bem como à Digital Innovation One (DIO) pela disponibilização do repositório [js-developer-pokedex](https://github.com/digitalinnovationone/js-developer-pokedex) que serviu como uma das bases para este projeto.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,26 @@
+let offset = 0
+const limit = 10
+const maxRecords = 151
+
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151
-const limit = 10
-let offset = 0;
-
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
-function loadPokemonItens(offset, limit) {
+function loadPokemonItens(offset, limit){
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        const newHtml = pokemons.map((pokemon) => `
+        <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+
+                <div class="detail">
+                    <ol class="types">
+                       ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+                    </ol>
+                    <img src="${pokemon.photo}"
+                         alt="${pokemon.name}">
+                </div>
+            </li>
+        `).join('')
         pokemonList.innerHTML += newHtml
     })
 }
@@ -34,10 +29,11 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+    
+    const qtdRecordWithNextPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
+    if(qtdRecordWithNextPage >= maxRecords){
+        const newLimit = maxRecords - offset 
         loadPokemonItens(offset, newLimit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
